5bf713d9e48e962840368fbfa731d3ab4d89a57a
Ashik6301 <ashiksk6301@gmail.com>
2023-09-07 14:37:09 +0600

    Fix Perf kernel and ksu suppor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone --depth 1 https://github.com/Ashik6301/Vendor-xiaomi-violet.git -b 13.0 vendor/xiaomi/violet
-git clone --depth 1 https://github.com/project-poison/android_kernel_xiaomi_violet.git -b perf kernel/xiaomi/violet
+git clone --depth 1 https://Ashik6301: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@github.com/project-poison/android_kernel_xiaomi_violet.git -b perf kernel/xiaomi/violet
 git clone --depth 1 https://gitlab.com/ashiksk6301/vendor_xiaomi-firmware_violet.git -b thirteen vendor/xiaomi-firmware/violet
 git clone --depth 1 https://gitlab.com/ashiksk6301/prebuilts_clang_host_linux-x86_clang-r498229.git -b master prebuilts/clang/host/linux-x86/clang-r498229
 git clone https://gitlab.com/ashiksk6301/android_vendor_miuicamera.git vendor/MiuiCamera
