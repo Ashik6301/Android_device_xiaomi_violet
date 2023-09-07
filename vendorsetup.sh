0a24bc46ea68510c19a6783602d908c4e01f1175
Ashik6301 <ashiksk6301@gmail.com>
2023-09-07 13:19:37 +0600

    added perf kernel and ksu support

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,9 @@
 git clone --depth 1 https://github.com/Ashik6301/Vendor-xiaomi-violet.git -b 13.0 vendor/xiaomi/violet
-git clone --depth 1 https://github.com/Ashik6301/Kernel-violet.git -b thirteen kernel/xiaomi/violet
+git clone --depth 1 https://github.com/project-poison/android_kernel_xiaomi_violet.git -b perf kernel/xiaomi/violet
 git clone --depth 1 https://gitlab.com/ashiksk6301/vendor_xiaomi-firmware_violet.git -b thirteen vendor/xiaomi-firmware/violet
 git clone --depth 1 https://gitlab.com/ashiksk6301/prebuilts_clang_host_linux-x86_clang-r498229.git -b master prebuilts/clang/host/linux-x86/clang-r498229
 git clone https://gitlab.com/ashiksk6301/android_vendor_miuicamera.git vendor/MiuiCamera
+git clone https://github.com/tiann/KernelSU.git -b main kernel/xiaomi/violet/KernelSU
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/media 
 rm -rf hardware/qcom-caf/sm8150/display
